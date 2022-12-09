b31d3f43e5c413621108805d254d771c9f79c81f
Solène Lefeu <solene.lefeu@epitech.eu>
2022-12-09 12:48:43 +0100

    [ADD] | Json component to handle information storage

---
+++ src/resumeData.jsx
@@ -0,0 +1,157 @@
+let resumeData = {
+  "name": "Solène Lefeu",
+  "role": "Portfolio",
+  "roleDescription": "Third-year Epitech student. French software engineer.",
+  "passions": "C++ devotee, Python afficionado and React enthousiast. Currently diving deeper into Object-Oriented programming with Java.",
+  "socialLinks": [
+    {
+      "name": "LinkedIn",
+      "url": "https://www.linkedin.com/in/solene-lefeu/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "Github",
+      "url": "http://github.com/slefeu",
+      "className": "fa fa-github"
+    },
+  ],
+  "aboutme": "I am currently Training for the title of Expert in Information Technology at Epitech. During my formation, I was able to acquire solid knowledges in C and C++ as well as in Python and React. On the side, I am expanding my knowledge by making mobile apps with Flutter and Dart. Along my studies, I also take part in the supervision of educational activities of our first and second year students. This involves monitoring their projects and morale.",
+  "education": [
+    {
+      "UniversityName": "Assomption Highschool, Level C1 ",
+      "specialization": "International Baccalaureate (British Option)",
+      "YearOfPassing": "2016",
+    },
+    {
+      "UniversityName": "Epitech School of Technology, RNCP title level 7 ",
+      "specialization": "Information and Communication Technology Expert",
+      "YearOfPassing": "2020 - ongoing",
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Freelance, private client",
+      "specialization": "Front-end mobile developer",
+      "MonthOfLeaving": "6 months -",
+      "YearOfLeaving": "2021",
+      "Achievements": "Implementation of the front-end of a mobile social network application with the Flutter framework. Self-taught Dart and Flutter Training ."
+    },
+    {
+      "CompanyName": "Epitech Bordeaux",
+      "specialization": "Regional Pedagogical Assistant (AER)",
+      "MonthOfLeaving": "2 years (ongoing) -",
+      "YearOfLeaving": "2022",
+      "Achievements": "Management and supervision of educational activities for 1st and 2nd year students. Support for learning C and C++. Live Coding and Code Review Sessions. Talks on Git tools, Linux, technical notions and SCRUM methods."
+    }
+  ],
+  "skills": [
+    {
+      "skillname": "C"
+    },
+    {
+      "skillname": "CPP"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "Flutter"
+    }
+  ],
+  "certification": [
+    {
+      "name": "Cambridge ESOL Certificate, Level B2 (English)",
+      "description": "First Certificate in English, Council of Europe",
+      "YearOfPassing": "2013",
+    },
+    {
+      "name": "Software Development Project Management",
+      "description": "IONISX Online Training ",
+      "YearOfPassing": "2022",
+    },
+    {
+      "name": "Crisis Communication Procedures",
+      "description": "IONISX Online Training ",
+      "YearOfPassing": "2022",
+    },
+    {
+      "name": "Team and Operational Management",
+      "description": "IONISX Online Training ",
+      "YearOfPassing": "2022 - ongoing",
+    },
+    {
+      "name": "Agile Coaching",
+      "description": "IONISX Online Training ",
+      "YearOfPassing": "2022 - ongoing",
+    },
+    {
+      "name": "Voltaire Certificate in Spelling (French)",
+      "description": "Projet Voltaire Online Training ",
+      "YearOfPassing": "2022 - ongoing",
+    },
+  ],
+
+  "portfolio": [
+    {
+      "name": "Plazza",
+      "description": "Simulation of a pizzeria in C++. Generic thread pool implementation for the cooks. C++ process and named pipe wrappers. Object serialization and deserialization.",
+      "imgurl": "images/plazza.png",
+      "link": {
+        "url": "https://github.com/slefeu/Plazza",
+        "name": "Github",
+        "icon": "fa fa-github"
+      }
+    },
+    {
+      "name": "Indie Studio",
+      "description": "Recreation of the cross-platform video game Bomberman in C++. Implementation of a local multi-player module. Creation of a generic entity component system (ECS) and game engine.",
+      "imgurl": "images/indie.png",
+      "link": {
+        "url": "https://github.com/slefeu/Bomberman",
+        "name": "Github",
+        "icon": "fa fa-github",
+      }
+    },
+    {
+      "name": "Area",
+      "description": "Dashboard-like dockerized web and mobile app. Implementation of a Rest API. Frontend made with ReactJS, backend made with Ruby on Rails (RoR).",
+      "imgurl": "images/area.png",
+      "link": {
+        "url": "https://github.com/slefeu/Area",
+        "name": "Github",
+        "icon": "fa fa-github"
+      }
+    },
+    {
+      "name": "Balm",
+      "description": "Mobile application featuring a new social network based on the user's localisation. Aimed to break social isolation and promote social interactions. Project made with Flutter and Dart.",
+      "imgurl": "images/balm.png",
+      "link": ""
+    },
+    {
+      "name": "SpaceMaze",
+      "description": "A co-op two players maze game made with Unity and C#. The player has to escape from a ship he's been locked in by solving enigmas in the rooms hidden by the labyrinth.",
+      "imgurl": "images/spacemaze.png",
+      "link": {
+        "url": "https://katsukumi.itch.io/spacemaze",
+        "name": "Play on Itch.io",
+        "icon": ""
+      }
+    },
+    {
+      "name": "Marvin Repair",
+      "description": "A 2D platform game about a student needing to repair his robot. Made with Unity and C#.",
+      "imgurl": "images/marvinrepair.png",
+      "link": {
+        "url": "https://solene-lefeu.itch.io/marvin-repair",
+        "name": "Play on Itch.io",
+        "icon": ""
+      }
+    }
+  ],
+}
+
+export default resumeData
